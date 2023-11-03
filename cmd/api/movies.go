f6331f0ef3d12e10a51a92063806e89568f8d1ba
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-03 08:57:58 -0500

    adding in helper function to be able to re-use across the code base for reading in ID parameter from the URL params request context.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,13 +12,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // showMovieHandler accepts GET requests with URL params or JSON payload /v1/movies/:id
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
-	// when httprouter is parsing a request, any interpolated URL params will be stored in the request context.
-	// We can use the ParamsFromContext() function to retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// We can then use the ByName() method to get the value of the "id" parameter which will always be a positive int.
-	// ByName() returns only string values so we need to convert it to an int with a base of 10, and 64 bits.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
