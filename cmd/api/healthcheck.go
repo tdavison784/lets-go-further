35e4f2de8a9ee46e4151045ae9c35d6287e6eca6
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-05 05:23:30 -0600

    added in a writeJSON helper function that we can continue to use across the code base to simplify our needs with marshalling JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // delcare a handler which writes plain text response with all needed info back to end user
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"Status": "Available", "Environment": %q, "Version": %q }`
-	js = fmt.Sprintf(js, app.config.env, version)
 
-	// Set content-type to application json on the response header
-	w.Header().Set("Content-Type", "application/json")
+	// create a map that has all healthcheck info
+	data := map[string]string{
+		"Status":      "Available",
+		"Environment": app.config.env,
+		"Version":     version,
+	}
 
-	// write the JSON body as a response
-	w.Write([]byte(js))
+	err := app.writeJSON(w, 200, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The application encountered a problem and could not complete your request",
+			http.StatusInternalServerError)
+	}
 }
