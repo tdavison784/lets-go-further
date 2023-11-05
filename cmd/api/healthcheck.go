b0770f9ba9976329ad9e9b02f3dcd463f4048e82
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-05 05:06:52 -0600

    changed our healthcheck.go handler to return a JSON structure

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,12 @@ import (
 
 // delcare a handler which writes plain text response with all needed info back to end user
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"Status": "Available", "Environment": %q, "Version": %q }`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set content-type to application json on the response header
+	w.Header().Set("Content-Type", "application/json")
+
+	// write the JSON body as a response
+	w.Write([]byte(js))
 }
