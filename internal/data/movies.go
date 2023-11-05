3ca90b9ee4fd608dc4fbb2e19e7f5cdd11e42577
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-05 05:49:36 -0600

    added in an ommitempty annotation for the CreatedAt field in the Movie struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,7 +4,7 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year"`
 	Runtime   int32     `json:"runtime"`
