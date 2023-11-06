cda8d91971e1898a0ad1f13d5c3bf302aa4fef64
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-06 05:36:39 -0600

    adding in an HTTP test file to test requests and adding in json.Decoder to decode clients JSON payload

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"greenlight.twd.net/internal/data"
 	"net/http"
 	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new movie")
+	// Declare an anonymous struct to hold the info we expect our clients to pass in the HTTP request body.
+	// Note the field names and types in the struct are a subset of the Movie struct we created earlier.
+	// This will be our *target decode destination*
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// init a new json.Decoder instance which rads from the request body and then uses the Decode() method to decode
+	// the body contents into the input struct as the target decode destination. If there was an error, we use the
+	// generic errorResponse() helper to send the client a 400 Bad Request response containing the error code
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump the contents of input struct into HTTP response
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": input}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // showMovieHandler accepts GET requests with URL params or JSON payload /v1/movies/:id
@@ -26,6 +49,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		Title:     "CasaBlanca",
 		Runtime:   102,
 		Genres:    []string{"romance", "drama", "war"},
+		Year:      1982,
 		Version:   1,
 	}
 
