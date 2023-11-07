718f23d95942e8676b7ec2081e039e81fb4541eb
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-07 04:56:13 -0600

    to keep our handler logic clean we moved the validation steps for our Movie struct into data/movies.go.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -23,26 +23,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	// Copy values from input struct into Movie struct
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
 	// Init new Validator instance
 	v := validator.New()
 
-	// Use the Check method to execute validation checks. This will add the provided key and error message
-	// to the errors map if the check does not evaluate to true.
-	v.Check(input.Title != "", "title", "title must not be empty")
-	v.Check(len(input.Title) <= 500, "title", "title must be less then 500 bytes long")
-
-	v.Check(input.Year != 0, "year", "must provide a year")
-	v.Check(input.Year >= 1888, "year", "year must be greater then 1888")
-	v.Check(input.Year <= int32(time.Now().Year()), "year", "year cannot be in the future")
-
-	v.Check(input.Genres != nil, "genres", "genres must be provided")
-	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(input.Genres) <= 5, "genres", "cannot contain more then 5 genres")
-	v.Check(validator.Unique(input.Genres), "genres", "cannot contain duplicate genres")
-
-	// Use the Valid() method to see if any checks failed. If they did, then use the failedValidationResponse
-	// helper to send a response to the client, passing in the v.Errors map
-	if !v.Valid() {
+	// Call the ValidateMovie() method and return a response containing the errors if any of the checks fail
+	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
