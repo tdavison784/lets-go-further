dfb9ad00a09d06edcba452f1325ab3f11aaef5d1
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-11 14:16:10 -0600

    adding in an HTTP test for using filters for more advanced movie lookup. Adding in some baseline code for instrumenting the filters usages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -203,3 +203,48 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) listMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// to keep things consistent with our other handlers, we'll define an input struct
+	// to hold the expected values from the request query string
+	var input struct {
+		Title    string
+		Genres   []string
+		Page     int
+		PageSize int
+		Sort     string
+	}
+
+	// create a new validator instance
+	v := validator.New()
+
+	// Call r.URL.Query() to get the url.Values map containing the query string data
+	qs := r.URL.Query()
+
+	// Use our helpers to extract the title and genres string values, failing back on defaults
+	// of an empty string and empty slice if they were not provided by the client
+	input.Title = app.readString(qs, "title", "")
+	input.Genres = app.readCSV(qs, "genres", []string{})
+
+	// get the page and page_size query string values as integers.
+	// notice we set the default page value to 1 and page_size to 20
+	// and that we pass the validator instance as the final argument here
+	input.Page = app.readInt(qs, "page", 1, v)
+	input.PageSize = app.readInt(qs, "page_size", 20, v)
+
+	// extract the sort query string value, falling back to "id" if it is not provided
+	// by the client (which will imply a ascending sort on movie ID)
+	input.Sort = app.readString(qs, "sort", "id")
+
+	// Check the validator instance for any errors and use the failedValidationResponse()
+	// helper to send the client a response if necessary
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err := app.writeJSON(w, http.StatusOK, input, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
