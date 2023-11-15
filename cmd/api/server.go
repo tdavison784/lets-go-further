bfc0189a064ebb9aae0a456410a7cde2c703b607
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-14 20:30:15 -0600

    adding in code to handle graceful shutdowns of the server. This implements a 30 second timeout context so instructs the http server to stop taking new requests and let living requests finish within the 30 second window.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -19,8 +24,57 @@ func (app *application) server() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
+	// create a graceful shutdownError channel. We will use this to receive any errors returned
+	// by the graceful Shutdown() function.
+	shutdownError := make(chan error)
+
+	// start a background goroutine
+	go func() {
+
+		// create a quit channel which carries os.Signal values
+		quit := make(chan os.Signal, 1)
+
+		// use signal.Notify() to listen for incoming SIGINT and SIGTERM signals
+		// and relay them to the quit channel. Any other signals will not be caught
+		// by signal.Notify() and will retain their default behavior
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until
+		// a signal is received.
+		s := <-quit
+
+		// log a message to say that the signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		// create a context with a 30-second timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		// Call Shutdown() on our server, passing in the context we just made.
+		// Shutdown() will return nil if the graceful shutdown was successful,
+		// or an error (which may happen because of a problem closing the listeners,
+		// or because the shutdown didn't complete before the 30-second context deadline
+		// is hit.). We relay this return value to the shutdownError channel.
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	// start the HTTP server
 	app.logger.Info("Starting Server", "addr", srv.Addr, "env", app.config.env)
 
-	return srv.ListenAndServe()
+	// Calling Shutdown() on our server will cause ListenAndServer() to immediately
+	// return a http.ErrServerClosed error. So if we see this error, it is actually a
+	// good thing and an indication that the graceful shutdown has started. So we check
+	// specifically for this, only returning the error if it is NOT http.ErrServerClosed
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// at this point we know the graceful shutdown completed successfully, and we
+	// log a "stopped server" message
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
 }
