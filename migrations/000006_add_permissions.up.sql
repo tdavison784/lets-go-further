325df269ce91dcd2e30c543cfd2776f6fdf0d0ba
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-23 05:31:59 -0600

    fixing formatting of SQL

--- migrations/000006_add_permissions.up.sql
+++ migrations/000006_add_permissions.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS permissions (
-                                           id bigserial PRIMARY KEY,
-                                           code text NOT NULL
+    id bigserial PRIMARY KEY,
+    code text NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users_permissions (
-                                                 user_id bigint NOT NULL REFERENCES users on DELETE CASCADE,
-                                                 permission_id bigint NOT NULL REFERENCES permissions on DELETE CASCADE,
-                                                 PRIMARY KEY (user_id, permission_id)
+    user_id bigint NOT NULL REFERENCES users on DELETE CASCADE,
+    permission_id bigint NOT NULL REFERENCES permissions on DELETE CASCADE,
+    PRIMARY KEY (user_id, permission_id)
 );
 
 INSERT INTO permissions (code)
