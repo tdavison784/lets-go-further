4434191cbc242a27227769e35c9a49648cf3456e
thomas.davison1@medica.com <thomas.davison1@medica.com>
2023-11-29 05:08:58 -0600

    added in custom metrics middleware to publish stats on total requests received, total responses sent, and total processing time in microseconds.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -33,5 +33,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/token", app.generateTokenHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authenticate", app.createAuthenticationTokenHandler)
 	router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
